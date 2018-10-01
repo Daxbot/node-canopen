c70362ff4f73eac8053498cd031d9aa0f6015ba6
Wilkins White <ww@novadynamics.com>
2018-10-01 15:02:34 -0700

    add get/set for dataObjects
    allows the user to use "device['section'] = value" syntax to update data in the object dictionary

--- Device.js
+++ Device.js
@@ -7,55 +7,120 @@ const PDO = require('./protocol/PDO');
 const NMT = require('./protocol/NMT');
 const EMCY = require('./protocol/Emergency');
 
-const dataTypeLookup = {
-    1:  'BOOLEAN',
-    2:  'INTEGER8',
-    3:  'INTEGER16',
-    4:  'INTEGER32',
-    5:  'UNSIGNED8',
-    6:  'UNSIGNED16',
-    7:  'UNSIGNED32',
-    8:  'REAL32',
-    9:  'VISIBLE_STRING',
-    10: 'OCTET_STRING',
-    11: 'UNICODE_STRING',
-    12: 'TIME_OF_DAY',
-    13: 'TIME_DIFFERENCE',
-    15: 'DOMAIN',
-    17: 'REAL64',
-    18: 'INTEGER40',
-    19: 'INTEGER48',
-    20: 'INTEGER56',
-    21: 'INTEGER64',
-    22: 'UNSIGNED24',
-    24: 'UNSIGNED40',
-    25: 'UNSIGNED48',
-    26: 'UNSIGNED56',
-    27: 'UNSIGNED64',
-    32: 'PDO_PARAMETER',
-    33: 'PDO_MAPPING',
-    34: 'SDO_PARAMETER',
-    35: 'IDENTITY',
+const dataTypes = {
+    BOOLEAN: 1,
+    INTEGER8: 2,
+    INTEGER16: 3,
+    INTEGER32: 4,
+    UNSIGNED8: 5,
+    UNSIGNED16: 6,
+    UNSIGNED32: 7,
+    REAL32: 8,
+    VISIBLE_STRING: 9,
+    OCTET_STRING: 10,
+    UNICODE_STRING: 11,
+    TIME_OF_DAY: 12,
+    TIME_DIFFERENCE: 13,
+    DOMAIN: 15,
+    REAL64: 17,
+    INTEGER40: 18,
+    INTEGER48: 19,
+    INTEGER56: 20,
+    INTEGER64: 21,
+    UNSIGNED24: 22,
+    UNSIGNED40: 24,
+    UNSIGNED48: 25,
+    UNSIGNED56: 26,
+    UNSIGNED64: 27,
+    PDO_PARAMETER: 32,
+    PDO_MAPPING: 33,
+    SDO_PARAMETER: 34,
+    IDENTITY: 35,
 };
 
-function parseDataType(type)
-{
-    return dataTypeLookup[parseInt(type)];
-}
-
-const objectTypeLookup = {
-    0: 'NULL',
-    2: 'DOMAIN',
-    5: 'DEFTYPE',
-    6: 'DEFSTRUCT',
-    7: 'VAR',
-    8: 'ARRAY',
-    9: 'RECORD',
+const objectTypes = {
+    NULL: 0,
+    DOMAIN: 2,
+    DEFTYPE: 5,
+    DEFSTRUCT: 6,
+    VAR: 7,
+    ARRAY: 8,
+    RECORD: 9,
 };
 
-function parseObjectType(type)
+function parseRaw(dataType, data)
 {
-    return objectTypeLookup[parseInt(type)];
+    let value, size, raw, buffer;
+
+    switch(dataType)
+    {
+        case dataTypes.BOOLEAN:
+            size = 1;
+            value = data ? (parseInt(data) != 0) : false;
+            raw = new Uint8Array( value ? [1] : [0] );
+            break;
+        case dataTypes.INTEGER8:
+        case dataTypes.UNSIGNED8:
+            size = 1;
+            value = data ? parseInt(data) : 0;
+            raw = new Uint8Array([value]);
+            break;
+        case dataTypes.INTEGER16:
+        case dataTypes.UNSIGNED16:
+            size = 2;
+            value = data ? parseInt(data) : 0;
+            buffer = new ArrayBuffer(size);
+            new Uint16Array(buffer).set([value]);
+            raw = new Uint8Array(buffer);
+            break;
+        case dataTypes.INTEGER32:
+        case dataTypes.UNSIGNED32:
+            size = 4;
+            value = data ? parseInt(data) : 0;
+            buffer = new ArrayBuffer(size);
+            new Uint32Array(buffer).set([value]);
+            raw = new Uint8Array(buffer);
+            break;
+        case dataTypes.UNSIGNED64:
+        case dataTypes.INTEGER64:
+            size = 8;
+            value = data ? parseInt(data) : 0;
+            raw = new Uint8Array(size);
+            raw[0] = value >> 54;
+            raw[1] = value >> 48;
+            raw[2] = value >> 40;
+            raw[3] = value >> 32;
+            raw[4] = value >> 24;
+            raw[5] = value >> 16;
+            raw[6] = value >> 8;
+            raw[7] = value >> 0;
+            break;
+        case dataTypes.REAL32:
+            size = 4;
+            value = data ? parseFloat(data) : 0.0;
+            buffer = new ArrayBuffer(size);
+            new Float32Array(buffer).set([value]);
+            raw = new Uint8Array(buffer);
+            break;
+        case dataTypes.REAL64:
+            size = 8;
+            value = data ? parseFloat(data) : 0.0;
+            buffer = new ArrayBuffer(size);
+            new Float64Array(buffer).set([value]);
+            raw = new Uint8Array(buffer);
+            break;
+        case dataTypes.OCTET_STRING:
+            value = raw = data ? Uint8Array.from(data) : new Uint8Array();
+            size = value.length;
+            break;
+
+        default:
+            value = data ? data : "";
+            raw = Uint8Array.from(value);
+            size = raw.length;
+    }
+
+    return [value, size, raw];
 }
 
 class Device extends EventEmitter
@@ -75,7 +140,7 @@ class Device extends EventEmitter
 
         channel.addListener("onMessage", this._onMessage, this);
 
-        let od = ini.parse(fs.readFileSync(edsPath, 'utf-8'));
+        const od = ini.parse(fs.readFileSync(edsPath, 'utf-8'));
         const indexMatch = RegExp('^[0-9A-Fa-f]{4}$')
         const subIndexMatch = RegExp('^([0-9A-Fa-f]{4})sub([0-9A-Fa-f]+)$')
 
@@ -83,102 +148,58 @@ class Device extends EventEmitter
         {
             if(indexMatch.test(section))
             {
-                let dataType = parseDataType(data.DataType);
-                let objectType = parseObjectType(data.ObjectType);
-                let value;
+                const dataType = parseInt(data.DataType);
+                const objectType = parseInt(data.ObjectType);
 
-                if(objectType == 'ARRAY' || objectType == 'RECORD')
+                let value, size, raw;
+                if(objectType == objectTypes.ARRAY 
+                || objectType == objectTypes.RECORD)
                 {
                     value = [];
+                    size = [];
+                    raw = [];
                 }
-                else switch(dataType)
-                {
-                    case 'BOOLEAN':
-                    case 'INTEGER8':
-                    case 'INTEGER16':
-                    case 'INTEGER32':
-                    case 'INTEGER64':
-                    case 'UNSIGNED8':
-                    case 'UNSIGNED16':
-                    case 'UNSIGNED32':
-                    case 'UNSIGNED64':
-                        if(data.DefaultValue)
-                            value = parseInt(data.DefaultValue);
-                        else
-                            value = 0;
-                        break;
-                    case 'REAL32':
-                    case 'REAL64':
-                        if(data.DefaultValue)
-                            value = parseFloat(data.DefaultValue);
-                        else
-                            value = 0.0;
-                        break;
-                    case 'OCTET_STRING':
-                        if(data.DefaultValue)
-                            value = Uint8ClampedArray.from(data.DefaultValue);
-                        else
-                            value = new Uint8ClampedArray();
-                        break;
-                    default:
-                        value = data.DefaultValue;
-                        break;
-                }
+                else
+                    [value, size, raw] = parseRaw(dataType, data.DefaultValue);
 
                 this.dataObjects[section] = {
-                    'name':         data.ParameterName,
-                    'dataType':     dataType,
-                    'objectType':   parseObjectType(data.ObjectType),
-                    'access':       (data.AccessType) ? data.AccessType : 'rw',
-                    'value':        value,
+                    name:       data.ParameterName,
+                    index:      parseInt(section, 16),
+                    dataType:   dataType,
+                    objectType: objectType,
+                    access:     (data.AccessType) ? data.AccessType : 'rw',
+                    value:      value,
+                    size:       size,
+                    raw:        raw,
                 };
 
-                this[data.ParameterName] = this.dataObjects[section];
+                Object.defineProperties(this, {
+                    [section]: {
+                        get: ()=>{
+                            return this.dataObjects[section];
+                        },
+                        set: (data)=>{
+                            const dataType = this.dataObjects[section].dataType;
+                            const [value, size, raw] = parseRaw(dataType, data);
+                            this.dataObjects[section].value = value;
+                            this.dataObjects[section].raw = raw;
+                            this.dataObjects[section].size = size;
+                        },
+                    },
+                });
             }
             else if(subIndexMatch.test(section))
             {
-                let [index, sub] = section.split('sub');
+                const [main, sub] = section.split('sub');
                 if(sub != '0')
                 {
-                    let dataType = parseDataType(data.DataType);
-                    let value;
+                    const dataType = parseInt(data.DataType);
+                    const [value, size, raw] = parseRaw(dataType, data.DefaultValue);
 
-                    switch(dataType)
-                    {
-                        case 'BOOLEAN':
-                        case 'INTEGER8':
-                        case 'INTEGER16':
-                        case 'INTEGER32':
-                        case 'INTEGER64':
-                        case 'UNSIGNED8':
-                        case 'UNSIGNED16':
-                        case 'UNSIGNED32':
-                        case 'UNSIGNED64':
-                            if(data.DefaultValue)
-                                value = parseInt(data.DefaultValue);
-                            else
-                                value = 0;
-                            break;
-                        case 'REAL32':
-                        case 'REAL64':
-                            if(data.DefaultValue)
-                                value = parseFloat(data.DefaultValue);
-                            else
-                                value = 0.0;
-                            break;
-                        case 'OCTET_STRING':
-                            if(data.DefaultValue)
-                                value = Uint8ClampedArray.from(data.DefaultValue);
-                            else
-                                value = new Uint8ClampedArray();
-                            break;
-                        default:
-                            value = data.DefaultValue;
-                            break;
-                    }
-
-                    this.dataObjects[index]['dataType'] = dataType;
-                    this.dataObjects[index].value[parseInt(sub)-1] = value; 
+                    this.dataObjects[main].dataType = dataType;
+                    this.dataObjects[main].value[parseInt(sub)-1] = value; 
+                    this.dataObjects[main].size[parseInt(sub)-1] = size; 
+                    this.dataObjects[main].raw[parseInt(sub)-1] = raw; 
                 }
             }
         }
@@ -195,28 +216,20 @@ class Device extends EventEmitter
         }
         if(msg.id == 0x580 + this.deviceId)
         {
-            // SDO
-            this.emit("SDO");
+            if(msg.data[0] == 0x80)
+                this.emit(
+                    "Abort"+msg.data.readUInt16LE(1).toString(16),
+                    msg.data.readUInt32LE(3)
+                );
+            else
+                this.emit("SDO"+msg.data.readUInt16LE(1).toString(16));
         }
-        else if(msg.id == 0x180 + this.deviceId)
+        else if(msg.id == 0x180 + this.deviceId
+             || msg.id == 0x280 + this.deviceId
+             || msg.id == 0x380 + this.deviceId
+             || msg.id == 0x480 + this.deviceId)
         {
-            // PDO0
-            this.emit("PDO0");
-        }
-        else if(msg.id == 0x280 + this.deviceId)
-        {
-            // PDO1
-            this.emit("PDO1");
-        }
-        else if(msg.id == 0x380 + this.deviceId)
-        {
-            // PDO2
-            this.emit("PDO2");
-        }
-        else if(msg.id == 0x480 + this.deviceId)
-        {
-            // PDO3
-            this.emit("PDO3");
+            this.emit("PDO"+msg.id.toString(16));
         }
         
     }
